92d12ed6831c2df49ae3371fdf7b4b140acc955c
dtrifonov100 <dtrifonov100@gmail.com>
2024-06-14 02:04:03 +0300

    README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Only express app
+
 Start: `npm run dev`
 
 Create todo: `curl -G -d "title=TODO_MY" http://localhost:8000/api/todos/create`
--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -3,14 +3,6 @@ const todosRoutes = require('./todos.routes');
 
 const apiRoutes = Router();
 
-const todos = [
-  {
-    id: 1,
-    title: 'Write code',
-    completed: false,
-  },
-];
-
 apiRoutes.use('/todos', todosRoutes);
 apiRoutes.get('/', (req, res) => {
   res.status(200).json({
